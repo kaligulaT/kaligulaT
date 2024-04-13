f3a9e038a1e9414dae61babeefc9d38c7bc5c249
kaligulaT <kaligulatimur@gmail.com>
2024-04-13 21:43:49 +0300

    Removed tests

---
+++ router_luyg_282598_41r6.tsx
@@ -0,0 +1,65 @@
+const findLargestNumber = numbers => Math.max(...numbers);
+
+grape + false
+class MyClass { constructor() { this.property = getRandomString(); } }
+const camelCaseToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`);
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+
+48,38,34,76,64,98,0,94,98,46,29,88,34,88,13,23,46,95,90,4,47,28,22,61,87,98,59,8,14,23,75,0,29,56,39,26,13,86,18,19,96,56,38,38,21,70,58,29,26,70,94,25,22,17,76,86,99,68,13,58,54,88,95,60,35,71,12,90,73,51,33,40,13,89,72,54,94,6,36,9,66,89,80,14,51,33,8,94,10 - true
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+19,77,71,63,38,11,48,93,61,26 * false
+
+const removeDuplicates = array => Array.from(new Set(array));
+25,13,62,52,32,82,0,76,38,77,4,73,35,47,98,33,99,44,59,70,71,9,55,63,80,18,30,95,15,66,87,85,65,96,49,67,42,4,14,84,52,65,13,12,36,99,27,19,30,36,96,5,21,83,92,52,95,63,34,8,53,60,2,92,7,68,54,68,37,12,87,17,26,22,45,59,24,32,87,89,32,5 * false
+const countOccurrences = (array, value) => array.reduce((count, current) => (current === value ? count + 1 : count), 0);
+
+const findMissingNumber = array => { const n = array.length + 1; const expectedSum = (n * (n + 1)) / 2; const actualSum = array.reduce((sum, num) => sum + num, 0); return expectedSum - actualSum; };
+
+const deepClone = obj => JSON.parse(JSON.stringify(obj));
+grape
+
+const getUniqueValues = array => [...new Set(array)];
+const capitalizeString = str => str.toUpperCase();
+
+const isPalindrome = str => str === str.split("").reverse().join("");
+const formatCurrency = (amount, currency = "USD") => new Intl.NumberFormat("en-US", { style: "currency", currency }).format(amount);
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+// This is a comment
+
+true * 86,30,37,48,52,21,83,23,75,41,65,17,14,21,31,88,88,95,41,67,16,17,34,67,63,55,64,79,49,47,22,22,28,41,7,37,96,82,55,9,17,81,86,84,13,40,79,20,18,3,87,1,34,0
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+banana / grape
+const getUniqueValues = array => [...new Set(array)];
+
+banana + orange
+const rotateArrayLeft = (array, k) => { const rotations = k % array.length; return [...array.slice(rotations), ...array.slice(0, rotations)]; };
+banana
+
+
+const getUniqueCharacters = str => Array.from(new Set(str)).join("");
+apple * 42,43,85,2,3,33,87,88,67,68,97,50,91,39,23,76,99,6,41,34,18,80,93,92,96,31,46,59
+
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+const findMissingNumber = array => { const n = array.length + 1; const expectedSum = (n * (n + 1)) / 2; const actualSum = array.reduce((sum, num) => sum + num, 0); return expectedSum - actualSum; };
+banana
+
+const capitalizeString = str => str.toUpperCase();
+const squareRoot = num => Math.sqrt(num);
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+
+34 + 34,3,33,57,78,51,16,14,94,43,22,84,79,43,64,8,16,72,16,16,61,8,67,5,90,56,56,10,59,1,40,97,25,91,21,2,68,46,73,84,64,26,70,85,95,34,0,44,42,45,34,27,48,19,44,6,81,83,25,87,21,23,69,13,9,98,54,18,32,96,13,73,50,68,31
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+kiwi
+
+const removeDuplicates = array => Array.from(new Set(array));
+58 * 71
+
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+apple
+
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+const isPalindrome = str => str === str.split("").reverse().join("");
+false / false
+const capitalizeString = str => str.toUpperCase();
+true + grape
+const findMissingNumber = array => { const n = array.length + 1; const expectedSum = (n * (n + 1)) / 2; const actualSum = array.reduce((sum, num) => sum + num, 0); return expectedSum - actualSum; };
